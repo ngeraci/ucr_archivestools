0b9874c4da1082db61613fbea129271ad685a884
Noah Geraci <noah.geraci@ucr.edu>
2022-12-02 12:35:49 -0800

    update packaging to use pyproject.toml, upgrade requirements

--- setup.py
+++ setup.py
@@ -1,16 +1,3 @@
-# -*- coding: utf-8 -*-
-from setuptools import setup, find_packages
-setup(
-    name='aspace_tools',
-    version="0.3",
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires=[
-        'requests', 'lxml', 'iso-639'
-    ],
-    entry_points={
-        'console_scripts': [
-            'oac-process = aspace_tools.oac_process:main',
-        ]
-    }
-)
+from setuptools import setup
+
+setup()
