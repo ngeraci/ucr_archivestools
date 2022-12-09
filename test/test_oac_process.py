26c2df01cbfce4fa5d4d319e8871c83f0ab888ad
Noah Geraci <noah.geraci@ucr.edu>
2022-12-09 11:41:50 -0800

    start working on tests

---
+++ test/test_oac_process.py
@@ -0,0 +1,17 @@
+import unittest
+
+class TestOACProcess(unittest.TestCase):
+
+    def simple_ead(self):
+        # ms040
+
+    def ead_with_title_emph(self):
+        # ms306
+
+    def ead_with_digital_object(self):
+        # ua392
+
+
+
+if __name__ == '__main__':
+    unittest.main()
